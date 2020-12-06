a9c012333bb83351ed56f9fa26e7ab7bc72e2798
guillaumedsde <guillaume@desusanne.com>
2020-12-06 14:18:37 +0100

    ci: switch to gitlab ci

---
+++ build/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VERSION=${VERSION:-"$(curl --silent "https://api.github.com/repos/boramalper/magnetico/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')"}
+
+
+if [ "${CI_COMMIT_REF_NAME}" = "master" ]; then
+    TAGS=" -t ${CI_REGISTRY_USER}/${CI_PROJECT_NAME}:${VERSION} -t ${CI_REGISTRY_USER}/${CI_PROJECT_NAME}:latest "
+else
+    # tag image with branch name
+    TAGS="-t ${CI_REGISTRY_USER}/${CI_PROJECT_NAME}:${CI_COMMIT_REF_SLUG}"
+fi
+
+# shellcheck disable=SC2086
+docker buildx build . \
+    --platform="${BUILDX_PLATFORM}" \
+    --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+    --build-arg VCS_REF="${CI_COMMIT_SHORT_SHA}" \
+    --build-arg MAGNETICOD_VERSION="${VERSION}"
+    ${TAGS} \
+    --push
